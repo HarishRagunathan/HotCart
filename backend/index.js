9ac9f7e31caa6020e141480e65571c04efe026e2
HarishRagunathan <harishragunathan758@gmail.com>
2025-04-01 07:48:32 +0530

    3

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ['https://hotcart.netlify.app'], // Allow only your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
+  }));
 
 // Routes
 
